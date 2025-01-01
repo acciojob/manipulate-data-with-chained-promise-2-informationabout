da2c67f048e6a7706770e5d122ab48b4380dcfbc
Coder <111493707+informationabout@users.noreply.github.com>
2025-01-01 19:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function getNumbers() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+function updateOutput(message) {
+    document.getElementById("output").textContent = message;
+}
+
+getNumbers()
+    .then((numbers) => {
+        const evenNumbers = numbers.filter(num => num % 2 === 0);
+        setTimeout(() => {
+            updateOutput("Even numbers: " + evenNumbers.join(", "));
+        }, 1000); 
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const doubledEvenNumbers = evenNumbers.map(num => num * 2);
+                resolve(doubledEvenNumbers);
+            }, 2000); 
+        });
+    })
+    .then((doubledNumbers) => {
+        updateOutput("Doubled even numbers: " + doubledNumbers.join(", "));
+    })
+    .catch((error) => {
+        console.error("An error occurred:", error);
+    });
