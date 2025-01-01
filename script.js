1ed4506ff78cb60983f2026d1495ca37c6316484
Coder <111493707+informationabout@users.noreply.github.com>
2025-01-01 20:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getNumbers() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve([1, 2, 3, 4]);
-        }, );
+        }, 0);
     });
 }
 function updateOutput(message) {
